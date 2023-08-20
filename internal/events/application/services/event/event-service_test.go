a633f83d9c7cd908a2fa2f09308c2c4a8b425542
gabrielsc1998 <98gabrielsc@gmail.com>
2023-08-20 16:42:21 -0300

    test: adjusts and adds tests in event service

--- internal/events/application/services/event/event-service_test.go
+++ internal/events/application/services/event/event-service_test.go
@@ -62,6 +62,57 @@ func TestShouldCreateAnEvent(t *testing.T) {
 	assert.Equal(t, partner.Id.Value, event[0].PartnerId.Value)
 }
 
+func TestShouldUpdateAnEvent(t *testing.T) {
+	Setup()
+
+	partner, _ := partner_entity.Create(partner_entity.PartnerCreateProps{
+		Id:   "",
+		Name: "Jhon Doe",
+	})
+	err := partnerRepository.Add(partner)
+	assert.Nil(t, err)
+
+	date := time.Now()
+	err = eventService.Create(event_dto.EventCreateDto{
+		Name:        "Event 01",
+		Description: "Event 01 description",
+		Date:        date,
+		PartnerId:   partner.Id.Value,
+	})
+	assert.Nil(t, err)
+
+	repo := event_repository.NewEventRepository(test.DB.DB)
+	events, err := repo.FindAll()
+
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(events))
+	assert.NotNil(t, events[0].Id.Value)
+	assert.Equal(t, "Event 01", events[0].Name)
+	assert.Equal(t, "Event 01 description", events[0].Description)
+	assert.Equal(t, date.Format("2006-01-02 15:04:05:000"), events[0].Date.Format("2006-01-02 15:04:05:000"))
+	assert.Equal(t, partner.Id.Value, events[0].PartnerId.Value)
+
+	date2 := time.Now()
+	err = eventService.Update(event_dto.EventUpdateDto{
+		Id:          events[0].Id.Value,
+		Name:        "Event 02",
+		Description: "Event 02 description",
+		Date:        date2,
+	})
+	assert.Nil(t, err)
+
+	event, err := repo.FindById(events[0].Id.Value)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "Event 02", event.Name)
+	assert.Equal(t, "Event 02 description", event.Description)
+	assert.Equal(t, date.Format("2006-01-02 15:04:05:000"), event.Date.Format("2006-01-02 15:04:05:000"))
+
+	events, err = repo.FindAll()
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(events))
+}
+
 func TestShouldGetAllEvents(t *testing.T) {
 	Setup()
 
@@ -92,12 +143,12 @@ func TestShouldGetAllEvents(t *testing.T) {
 	events, err := eventService.FindEvents()
 	assert.Nil(t, err)
 	assert.Equal(t, 2, len(events))
-	assert.Equal(t, "Event 01", events[1].Name)
-	assert.Equal(t, "Event 02", events[0].Name)
-	assert.Equal(t, "Event 01 description", events[1].Description)
-	assert.Equal(t, "Event 02 description", events[0].Description)
-	assert.Equal(t, date.Format("2006-01-02 15:04:05:000"), events[1].Date.Format("2006-01-02 15:04:05:000"))
-	assert.Equal(t, date.Format("2006-01-02 15:04:05:000"), events[0].Date.Format("2006-01-02 15:04:05:000"))
-	assert.Equal(t, partner.Id.Value, events[1].PartnerId.Value)
+	assert.Equal(t, "Event 01", events[0].Name)
 	assert.Equal(t, partner.Id.Value, events[0].PartnerId.Value)
+	assert.Equal(t, "Event 01 description", events[0].Description)
+	assert.Equal(t, date.Format("2006-01-02 15:04:05:000"), events[0].Date.Format("2006-01-02 15:04:05:000"))
+	assert.Equal(t, "Event 02", events[1].Name)
+	assert.Equal(t, "Event 02 description", events[1].Description)
+	assert.Equal(t, date.Format("2006-01-02 15:04:05:000"), events[1].Date.Format("2006-01-02 15:04:05:000"))
+	assert.Equal(t, partner.Id.Value, events[1].PartnerId.Value)
 }
