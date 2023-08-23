aa22942412db9d09f292ed5476bbb6d39c1334d3
gabrielsc1998 <98gabrielsc@gmail.com>
2023-08-23 14:50:29 -0300

    feat: implements application service

---
+++ internal/common/application/application-service/application-service.go
@@ -0,0 +1,40 @@
+package application_service
+
+import (
+	domain_event_manager "github.com/gabrielsc1998/go-ddd/internal/common/domain/domain-event-manager"
+	"github.com/gabrielsc1998/go-ddd/internal/common/domain/entity"
+)
+
+type ApplicationServiceInterface interface {
+	Start()
+	Fail()
+	Run(aggregateRoots []*entity.AggregateRoot, callback func() error) error
+}
+
+type ApplicationService struct {
+	domainEventManager domain_event_manager.DomainEventManagerInterface
+}
+
+func NewApplicationService(domainEventManager domain_event_manager.DomainEventManagerInterface) *ApplicationService {
+	return &ApplicationService{
+		domainEventManager: domainEventManager,
+	}
+}
+
+func (a *ApplicationService) Start() {
+}
+
+func (a *ApplicationService) Fail() {
+}
+
+func (a *ApplicationService) Run(aggregateRoots []*entity.AggregateRoot, callback func() error) error {
+	err := callback()
+	if err != nil {
+		a.Fail()
+		return err
+	}
+	for _, aggregateRoot := range aggregateRoots {
+		a.domainEventManager.Publish(aggregateRoot)
+	}
+	return nil
+}
