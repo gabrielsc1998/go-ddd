7a7f310ea3f838acb81b8a684347b09e769f36fa
gabrielsc1998 <98gabrielsc@gmail.com>
2023-08-23 14:46:55 -0300

    chore: updates aggregate root using domain event

--- internal/common/domain/entity/aggregate-root.go
+++ internal/common/domain/entity/aggregate-root.go
@@ -1,23 +1,29 @@
 package entity
 
-import "github.com/gabrielsc1998/go-ddd/internal/common/domain/value-objects/id"
+import (
+	domain_event "github.com/gabrielsc1998/go-ddd/internal/common/domain/domain-event"
+	"github.com/gabrielsc1998/go-ddd/internal/common/domain/value-objects/id"
+)
 
 type AggregateRootInterface interface {
+	AddEvent(event domain_event.DomainEvent)
+	ClearEvents()
+	GetEvents() []domain_event.DomainEvent
 }
 
 type AggregateRoot struct {
 	Id     *id.ID
-	events []interface{}
+	events []domain_event.DomainEvent
 }
 
-func (a *AggregateRoot) AddEvent(event interface{}) {
+func (a *AggregateRoot) AddEvent(event domain_event.DomainEvent) {
 	a.events = append(a.events, event)
 }
 
 func (a *AggregateRoot) ClearEvents() {
-	a.events = []interface{}{}
+	a.events = []domain_event.DomainEvent{}
 }
 
-func (a *AggregateRoot) GetEvents() []interface{} {
+func (a *AggregateRoot) GetEvents() []domain_event.DomainEvent {
 	return a.events
 }
