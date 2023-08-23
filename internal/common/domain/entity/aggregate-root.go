099e8fdb5a0a05f38a27cd942b34ab50c4249e31
gabrielsc1998 <98gabrielsc@gmail.com>
2023-08-23 08:46:40 -0300

    feat: adds domain event to aggregate root

--- internal/common/domain/entity/aggregate-root.go
+++ internal/common/domain/entity/aggregate-root.go
@@ -6,5 +6,18 @@ type AggregateRootInterface interface {
 }
 
 type AggregateRoot struct {
-	Id *id.ID `json:"id"`
+	Id     *id.ID
+	events []interface{}
+}
+
+func (a *AggregateRoot) AddEvent(event interface{}) {
+	a.events = append(a.events, event)
+}
+
+func (a *AggregateRoot) ClearEvents() {
+	a.events = []interface{}{}
+}
+
+func (a *AggregateRoot) GetEvents() []interface{} {
+	return a.events
 }
