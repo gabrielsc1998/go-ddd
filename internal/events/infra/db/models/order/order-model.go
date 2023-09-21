6f7e900993ae2271a10d4ecad2cd2403986d2e52
gabrielsc1998 <98gabrielsc@gmail.com>
2023-09-20 21:04:02 -0300

    chore: updates models foreing keys

--- internal/events/infra/db/models/order/order-model.go
+++ internal/events/infra/db/models/order/order-model.go
@@ -1,15 +1,19 @@
 package order_model
 
 import (
+	event_model "github.com/gabrielsc1998/go-ddd/internal/events/infra/db/models/event"
+	spot_model "github.com/gabrielsc1998/go-ddd/internal/events/infra/db/models/spot"
 	"gorm.io/gorm"
 )
 
 type Order struct {
 	gorm.Model
-	ID          string  `gorm:"primaryKey;type:varchar(36);"`
-	CustomerId  string  `gorm:"type:varchar(36);"`
-	Amount      float64 `gorm:"not null"`
-	EventId     string  `gorm:"type:varchar(36);"`
-	EventSpotId string  `gorm:"type:varchar(36);"`
-	Status      int     `gorm:"not null;default:0"`
+	ID          string            `gorm:"primaryKey;type:varchar(36);"`
+	CustomerId  string            `gorm:"type:varchar(36);"`
+	Amount      float64           `gorm:"not null"`
+	EventId     string            `gorm:"type:varchar(36);"`
+	Event       event_model.Event `gorm:"foreignKey:EventId"`
+	EventSpotId string            `gorm:"type:varchar(36);"`
+	Spot        spot_model.Spot   `gorm:"foreignKey:EventSpotId"`
+	Status      int               `gorm:"not null;default:0"`
 }
--- internal/events/infra/db/models/spot-reservation/spot-reservation-model.go
+++ internal/events/infra/db/models/spot-reservation/spot-reservation-model.go
@@ -3,13 +3,15 @@ package spot_reservation_model
 import (
 	"time"
 
+	spot_model "github.com/gabrielsc1998/go-ddd/internal/events/infra/db/models/spot"
 	"gorm.io/gorm"
 )
 
 type SpotReservation struct {
 	gorm.Model
-	ID              string    `gorm:"primaryKey;type:varchar(36);"`
-	CustomerId      string    `gorm:"type:varchar(36);"`
-	SpotId          string    `gorm:"type:varchar(36);"`
-	ReservationDate time.Time `gorm:"not null"`
+	ID              string          `gorm:"primaryKey;type:varchar(36);"`
+	CustomerId      string          `gorm:"type:varchar(36);"`
+	SpotId          string          `gorm:"type:varchar(36);"`
+	Spot            spot_model.Spot `gorm:"foreignKey:SpotId;references:ID"`
+	ReservationDate time.Time       `gorm:"not null"`
 }
